7ff27c5736d67424305f6f554a1cac41aaac998d
Administrator <andrew.macaulay@team.telstra.com>
2022-08-31 08:50:52 +1000

    spellchecker

--- spell-check.sh
+++ spell-check.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md" | buildkite-agent annotate
+docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md" \
+  | sed '1s/^/```term\n/' \
+  | sed '$s/$/\n```/' \
+  | buildkite-agent annotate
 # image exits with 1 when there are errors
 exit 0
