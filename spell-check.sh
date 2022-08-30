170b1c5c20fd4c5168c0298594b7711339097efc
Administrator <andrew.macaulay@team.telstra.com>
2022-08-30 10:56:56 +1000

    fix spell check

--- spell-check.sh
+++ spell-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-output=${docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"}  
+PWD=${pwd}
+output=${docker run --rm -ti -v $PWD:/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"} $ 
 echo $output
 echo -e "\`\`\`term\n$output \033[0;31mtest\033[0m\n\`\`\`" | buildkite-agent annotate
 # image exits with 1 when there are errors
