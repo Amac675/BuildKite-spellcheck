2094b80709c62a61a2fb0f39e925ebfd48190d91
Administrator <andrew.macaulay@team.telstra.com>
2022-08-30 11:09:24 +1000

    fix spell check

--- spell-check.sh
+++ spell-check.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-docker run --rm -ti -v ${pwd}:/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"   
+docker run --rm -ti -v $(pwd):/workdir tmaier/markdown-spellcheck:latest --report "**/*.md"
 # image exits with 1 when there are errors
 exit 0
